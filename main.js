4cb8dd884debe1eb3ea60d4acb90e8b46240a384
Darren Thomas Clark <darren.clark@live.com>
2016-08-16 10:05:51 -0400

    final/only commit.

---
+++ main.js
@@ -0,0 +1,35 @@
+for (var i = 0; i < array.length; i++) {
+  console.log(array[i])
+}
+
+array.forEach((n) => {
+  console.log(n, n *n)
+  // return n * // NOTE:
+})
+
+console.log(each)
+
+const squares = array.map((n) => {
+  return n * n
+})
+
+console.log(squares)
+
+const numbers = [4, 8, 12, 15, 16, 23, 42]
+
+const odds = numbers.filter((n) => {
+  return n % 2 === 1
+})
+
+console.log(odds)
+
+const sum = numbers.reduce((total, n) => {
+  console.log({total, n})
+  return total + n
+}
+const sum = numbers.reduce(f,0)
+
+console.log(sum)
+
+let t = 0
+for (let i = 0; i < numbers.length; i <=> )
